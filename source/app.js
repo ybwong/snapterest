1eb48302b295eaa37444b8f9f6596656e7cc9cf3
ybwong <philip_ybwong@yahoo.com>
2017-02-27 08:33:50 -0800

    Chapter 5 & 6

--- source/app.js
+++ source/app.js
@@ -3,31 +3,37 @@ var ReactDOM = require('react-dom');
 
 // Chapter 1 & Chapter 2
 //
-// var reactElement = React.createElement('h1', { className: 'header' }, 'This is React');
+// var reactElement = React.createElement('h1', {
+//   className: 'header'
+// }, 'This is React');
 // ReactDOM.render(reactElement, document.getElementById('react-application'));
 
 // var listItemElement1 = React.DOM.li({
-//     className: 'item-1',
-//     key: 'item-1'
+//   className: 'item-1',
+//   key: 'item-1'
 // }, 'Item 1');
 // var listItemElement2 = React.DOM.li({
-//     className: 'item-2',
-//     key: 'item-2'
+//   className: 'item-2',
+//   key: 'item-2'
 // }, 'Item 2');
 // var listItemElement3 = React.DOM.li({
-//     className: 'item-3',
-//     key: 'item-3'
+//   className: 'item-3',
+//   key: 'item-3'
 // }, 'Item 3');
 // var reactFragment = [listItemElement1, listItemElement2,
-//     listItemElement3
+//   listItemElement3
 // ];
-// var listOfItems = React.DOM.ul({ className: 'list-of-items' }, reactFragment);
+// var listOfItems = React.DOM.ul({
+//   className: 'list-of-items'
+// }, reactFragment);
 
-// var listOfItems = <ul className="list-of-items">
-//                        <li className="item-1">Item 1</li>
-//                        <li className="item-2">Item 2</li>
-//                        <li className="item-3">Item 3</li>
-//                   </ul>;
+// var listOfItems = (
+//     <ul className="list-of-items">
+//         <li className="item-1"> Item 1 </li>
+//         <li className="item-2"> Item 2 </li>
+//         <li className="item-3"> Item 3 </li>
+//     </ul>
+// );
 
 // ReactDOM.render(listOfItems, document.getElementById('react-application'));
 
@@ -35,42 +41,51 @@ var ReactDOM = require('react-dom');
 //
 // var ReactTestClass = React.createClass({
 //   render: function() {
-//     return React.createElement('h1', { className: 'header' }, 'React Component');
+//     return React.createElement('h1', {
+//       className: 'header'
+//     }, 'React Component');
 //   }
 // });
 // var reactComponentElement = React.createElement(ReactTestClass);
 // var reactComponent = ReactDOM.render(reactComponentElement, document.getElementById('react-application'));
 
-//
-var reactClass = React.createClass({
-  getInitialState: function() {
-    return {
-      isHeaderHidden: false
-    };
-  },
-  handleClick: function() {
-    this.setState({
-      isHeaderHidden: !this.state.isHeaderHidden
-    });
-  },
-  render: function() {
-    var title = 'Stateful React Component';
-    var headerElement = React.createElement('h1', {
-      className: 'header',
-      key: 'header'
-    }, title);
-    var buttonElement = React.createElement('button', {
-      className: 'btn btn-default',
-      onClick: this.handleClick,
-      key: 'button'
-    }, 'Toggle header');
-    if (this.state.isHeaderHidden) {
-      return React.createElement('div', null, [buttonElement]);
-    }
-    return React.createElement('div', null, [buttonElement,
-      headerElement
-    ]);
-  }
-});
-var reactComponentElement = React.createElement(reactClass);
-var reactComponent = ReactDOM.render(reactComponentElement, document.getElementById('react-application'));
+
+// var reactClass = React.createClass({
+//   getInitialState: function() {
+//     return {
+//       isHeaderHidden: false
+//     };
+//   },
+//   handleClick: function() {
+//     this.setState({
+//       isHeaderHidden: !this.state.isHeaderHidden
+//     });
+//   },
+//   render: function() {
+//     var title = 'Stateful React Component';
+//     var headerElement = React.createElement('h1', {
+//       className: 'header',
+//       key: 'header'
+//     }, title)
+//     var buttonElement = React.createElement('button', {
+//       className: 'btn btn-default',
+//       onClick: this.handleClick,
+//       key: 'button'
+//     }, 'Toggle header');
+//     if (this.state.isHeaderHidden) {
+//       return React.createElement('div', null, [buttonElement]);
+//     }
+//     return React.createElement('div', null, [buttonElement,
+//       headerElement
+//     ]);
+//   }
+// });
+// var reactComponentElement = React.createElement(reactClass);
+// var reactComponent = ReactDOM.render(reactComponentElement, document.getElementById('react-application'));
+
+
+// Chapter 4
+var Application = require('./components/Application.react');
+
+ReactDOM.render(<Application />, document.getElementById('react-application'));
+
