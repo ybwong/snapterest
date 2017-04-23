c16e00d4018e8cce815b80f13d5e8cbc99c0e8f6
ybwong <ybwong@cisco.com>
2017-04-23 22:15:35 +0800

    page 165

--- source/components/Stream.react.js
+++ source/components/Stream.react.js
@@ -1,35 +1,46 @@
 var React = require("react");
-var SnapkiteStreamClient = require("snapkite-stream-client");
+// var SnapkiteStreamClient = require("snapkite-stream-client");
 var StreamTweet = require("./StreamTweet.react");
 var Header = require("./Header.react");
+var TweetStore = require('../stores/TweetStore');
+
 var Stream = React.createClass({
   getInitialState: function() {
     return {
-      tweet: null
+      // tweet: null
+      tweet: TweetStore.getTweet()
     };
   },
+
   componentDidMount: function() {
-    SnapkiteStreamClient.initializeStream(this.handleNewTweet);
+    // SnapkiteStreamClient.initializeStream(this.handleNewTweet);
+    TweetStore.addChangeListener(this.onTweetChange);
   },
   componentWillUnmount: function() {
-    SnapkiteStreamClient.destroyStream();
+    // SnapkiteStreamClient.destroyStream();
+    TweetStore.removeChangeListener(this.onTweetChange);
   },
-  handleNewTweet: function(tweet) {
+  // handleNewTweet: function(tweet) {
+  //   this.setState({
+  //     tweet: tweet
+  //   });
+  // },
+  onTweetChange: function() {
     this.setState({
-      tweet: tweet
+      tweet: TweetStore.getTweet()
     });
   },
+
   render: function() {
     var tweet = this.state.tweet;
     if (tweet) {
-      return (
-        <StreamTweet
-          tweet={tweet}
-          onAddTweetToCollection={this.props.onAddTweetToCollection}
-        />
-      );
+      // return ( < StreamTweet tweet = { tweet }
+      //   onAddTweetToCollection = { this.props.onAddTweetToCollection }
+      //   />
+      // );
+      return (<StreamTweet tweet = {tweet}/>);
     }
-    return <Header text="Waiting for public photos from Twitter..." />;
+    return <Header text = "Waiting for public photos from Twitter..." / > ;
   }
 });
 module.exports = Stream;
